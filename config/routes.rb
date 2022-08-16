6e069593af0f3c47869ad7fdc911353874aaa834
Maya Eyal <mayalindae@gmail.com>
2022-08-16 12:27:59 +0200

    created show route and updated link from index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'tasks', to: 'tasks#index', as: :tasks
+
+  get 'tasks/:id', to: 'tasks#show', as: :show
   # Defines the root path route ("/")
   # root "articles#index"
 end
