c5904d3ec4bc68a795db4d1ef5bb4e914e1b0954
Maya Eyal <mayalindae@gmail.com>
2022-08-16 12:59:59 +0200

    added create task route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   get 'tasks', to: 'tasks#index', as: :tasks
 
-  get 'tasks/:id', to: 'tasks#show', as: :show
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  post 'tasks', to: 'tasks#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
